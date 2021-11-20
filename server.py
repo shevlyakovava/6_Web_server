0986786e1a98dbe42153deeaabca6d0e54d90b9e
shevlyakovava <90023991+shevlyakovava@users.noreply.github.com>
2021-11-20 14:54:20 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,31 +1,35 @@
 import socket
+from threading import Thread
 
+def working(conn, addr):
+    print(f'{addr} was connected')
+
+    user = conn.recv(1024).decode()
+    print(user)
+    request_from_server = user.split(" ")[1]
+    if request_from_server == '/':
+        with open('index.html', 'rb') as file:
+            site = file.read()
+            conn.send(site)
+    if request_from_server == '/index.html':
+        with open('index.html', 'rb') as file:
+            site = file.read()
+            conn.send(site)
+
+    else:
+        with open('error.html', 'rb') as file:
+            site = file.read()
+            conn.send(site)
+
+            
 sock = socket.socket()
-
 try:
     sock.bind(('', 80))
-    print("Using port 80")
 except OSError:
     sock.bind(('', 8080))
-    print("Using port 8080")
-
+print("Сервер запущен")
 sock.listen(5)
-
-conn, addr = sock.accept()
-print("Connected", addr)
-
-data = conn.recv(8192)
-msg = data.decode()
-
-print(msg)
-
-resp = """HTTP/1.1 200 OK
-Server: SelfMadeServer v0.0.1
-Content-type: text/html
-Connection: close
-
-Hello, webworld!"""
-
-conn.send(resp.encode())
-
-conn.close()
+while True:
+    conn, addr = sock.accept()
+    thread = Thread(target=working, args=(conn, addr,))
+    thread.start()
